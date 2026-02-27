df28c559143619ed7f2aea3176b28379956a8452
tthandi <44735287+tthandi@users.noreply.github.com>
2026-02-26 18:03:48 -0800

    Removed Project ID

--- sql/queries/Monthly Returns Search.sql
+++ sql/queries/Monthly Returns Search.sql
@@ -1,4 +1,4 @@
-DECLARE formation_date DATE DEFAULT DATE '2020-01-31';
+DECLARE formation_date DATE DEFAULT DATE '';
 DECLARE top_n INT64 DEFAULT 30;
 
 WITH
@@ -12,7 +12,7 @@ ranked_universe AS (
     ROW_NUMBER() OVER (
       ORDER BY ey DESC, roc DESC, symbol
     ) AS mf_rank
-  FROM `fluid-terminal-465516-s7.magic_formula.market_magic_formula_values_with_exclusions`
+  FROM `${PROJECT_ID}.magic_formula.market_magic_formula_values_with_exclusions`
   WHERE qdate = formation_date
 ),
 
@@ -31,7 +31,7 @@ buy_dates AS (
     u.formation_date,
     MIN(dp.date) AS buy_date
   FROM universe u
-  JOIN `fluid-terminal-465516-s7.magic_formula.daily_price` dp
+  JOIN `${PROJECT_ID}.magic_formula.daily_price` dp
     ON dp.symbol = u.symbol
    AND dp.date > u.formation_date
   GROUP BY u.symbol, u.mf_rank, u.formation_date
@@ -46,7 +46,7 @@ sell_dates AS (
     b.buy_date,
     MIN(dp.date) AS sell_date
   FROM buy_dates b
-  JOIN `fluid-terminal-465516-s7.magic_formula.daily_price` dp
+  JOIN `${PROJECT_ID}.magic_formula.daily_price` dp
     ON dp.symbol = b.symbol
    AND dp.date >= DATE_ADD(b.formation_date, INTERVAL 1 YEAR)
   GROUP BY b.symbol, b.mf_rank, b.formation_date, b.buy_date
@@ -63,9 +63,9 @@ priced AS (
     sp.adj_close AS sell_px,
     SAFE_DIVIDE(sp.adj_close, bp.adj_close) - 1 AS stock_return
   FROM sell_dates s
-  JOIN `fluid-terminal-465516-s7.magic_formula.daily_price` bp
+  JOIN `${PROJECT_ID}.magic_formula.daily_price` bp
     ON bp.symbol = s.symbol AND bp.date = s.buy_date
-  JOIN `fluid-terminal-465516-s7.magic_formula.daily_price` sp
+  JOIN `${PROJECT_ID}.magic_formula.daily_price` sp
     ON sp.symbol = s.symbol AND sp.date = s.sell_date
 )
 
